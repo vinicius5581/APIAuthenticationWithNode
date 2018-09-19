b602bb10127163afbe50f31717dffc5218ab9f6e
Vinicius Santana <santanaviny@gmail.com>
2018-09-18 19:45:22 -0700

    nem sei

--- server/configuration/index.js
+++ server/configuration/index.js
@@ -1,29 +1,13 @@
-if (process.env.NODE_ENV === 'test') {
-  module.exports = {
-    JWT_SECRET: 'codeworkrauthentication',
-    oauth: {
-      google: {
-        clientID: 'number',
-        clientSecret: 'string',
-      },
-      facebook: {
-        clientID: 'number',
-        clientSecret: 'string',
-      },
+module.exports = {
+  JWT_SECRET: 'something',
+  oauth: {
+    google: {
+      clientID: '29040190555-ttm7gsil8235qaqdkk1vfh7u7stuhqq5.apps.googleusercontent.com',
+      clientSecret: 'lWzDEfhxlaCQ3NSaXJ2iKAiA'
     },
-  };
-} else {
-  module.exports = {
-    JWT_SECRET: '',
-    oauth: {
-      google: {
-        clientID: '',
-        clientSecret: '',
-      },
-      facebook: {
-        clientID: '',
-        clientSecret: '',
-      },
-    },
-  };
-}
+    facebook: {
+      clientID: '384791728722741',
+      clientSecret: 'c155fcbd92560b4eea9b705bea3e6df5'
+    }
+  }
+};
